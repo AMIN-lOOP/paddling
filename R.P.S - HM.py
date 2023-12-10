b77e25bcccc8c6157fcc016b4964152a4d4f2fd1
AMIN-lOOP <153190467+AMIN-lOOP@users.noreply.github.com>
2023-12-11 02:52:47 +0330

    Add files via upload

---
+++ R.P.S - HM.py
@@ -0,0 +1,56 @@
+import random
+print ( "ROCK - PAPER - SCISSOR" )  
+
+print ( " " )
+
+p1 = input( " PL-1 move : " )
+p2 = input( " PL-2 move : " )
+if p1 == "R" and p2 == "P" :
+
+    print ( "p2 win" )
+
+if p1 == "R" and p2 == "S" :
+
+    print ( "p1 win" )
+    
+if p1 == "R" and p2 == "R" :
+    
+    print ( "Two players tied" )
+    
+if p1 == "P" and p2 == "P" :
+
+    print ( "Two players tied" )
+    
+if p1 == "P" and p2 == "R" :
+
+    print ( "p1 win" )
+    
+if p1 == "P" and p2 == "S" :
+
+    print ( "p2 win" )
+    
+if p1 == "S" and p2 == "S" :
+
+    print ( "Two players tied" )
+    
+if p1 == "S" and p2 == "P" :
+
+    print ( "p2 win" )
+    
+if p1 == "S" and p2 == "R" :
+
+    print ( "p2 win" )
+    
+if p1 == "R" and p2 == "P" :
+
+    print ( "p2 win" )
+    
+if p1 == "R" and p2 == "P" :
+
+    print ( "p2 win" )
+    
+if p1 == "R" and p2 == "P" :
+                                                
+    print ( "p2 win" )
+
+
