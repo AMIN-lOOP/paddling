8f9c5deaade6c67348243acd64e588453068dcde
AMIN-lOOP <153190467+AMIN-lOOP@users.noreply.github.com>
2023-12-11 13:30:13 +0330

    Update ROCK-PAPER-SCISSORS.py

--- ROCK-PAPER-SCISSORS.py
+++ ROCK-PAPER-SCISSORS.py
@@ -1,56 +1,18 @@
 import random
-print ( "ROCK - PAPER - SCISSOR" )  
 
-print ( " " )
-
-p1 = input( " PL-1 move : " )
-p2 = input( " PL-2 move : " )
-if p1 == "R" and p2 == "P" :
-
-    print ( "p2 win" )
-
-if p1 == "R" and p2 == "S" :
-
-    print ( "p1 win" )
-    
-if p1 == "R" and p2 == "R" :
-    
-    print ( "Two players tied" )
-    
-if p1 == "P" and p2 == "P" :
-
-    print ( "Two players tied" )
-    
-if p1 == "P" and p2 == "R" :
-
-    print ( "p1 win" )
-    
-if p1 == "P" and p2 == "S" :
-
-    print ( "p2 win" )
-    
-if p1 == "S" and p2 == "S" :
-
-    print ( "Two players tied" )
-    
-if p1 == "S" and p2 == "P" :
-
-    print ( "p2 win" )
-    
-if p1 == "S" and p2 == "R" :
-
-    print ( "p2 win" )
-    
-if p1 == "R" and p2 == "P" :
-
-    print ( "p2 win" )
-    
-if p1 == "R" and p2 == "P" :
-
-    print ( "p2 win" )
-    
-if p1 == "R" and p2 == "P" :
-                                                
-    print ( "p2 win" )
+print("ROCK - PAPER - SCISSOR")
+print(" ")
 
 
+p1 = input("PL-1 move (R/P/S): ").upper()
+
+p2 = random.choice(['R', 'P', 'S'])
+
+print(f"PL-2 move: {p2}")
+
+if p1 == p2:
+    print("Two players tied")
+elif (p1 == "R" and p2 == "S") or (p1 == "P" and p2 == "R") or (p1 == "S" and p2 == "P"):
+    print("p1 wins")
+else:
+    print("p2 wins")
